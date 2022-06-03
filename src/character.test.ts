03c86d312e30f3aed405350cfd94b0985dd2dc7e
Takeda Takumi <t.takeda@guncys.com>
2022-06-03 11:36:33 +0900

    feat: パーサ関数を生成する関数を実装した。

---
+++ src/character.test.ts
@@ -0,0 +1,112 @@
+import { character, is } from './character';
+import type { ParserOutput } from 'types';
+
+describe('character("a")', () => {
+	const parser = character('a');
+
+	test('Empty input', () => {
+		const input = [] as const;
+		const output = parser(input);
+		expect(output).toEqual<ParserOutput<'a'>>({
+			result: 'fail',
+		});
+	});
+	test('input "a"', () => {
+		const input = [...'a'];
+		const output = parser(input);
+		expect(output).toEqual<ParserOutput<'a'>>({
+			result: 'success',
+			data: 'a',
+			rest: [],
+		});
+	});
+	test('input "A"', () => {
+		const input = [...'A'];
+		const output = parser(input);
+		expect(output).toEqual<ParserOutput<'a'>>({
+			result: 'fail',
+		});
+	});
+	test('input "hoge"', () => {
+		const input = [...'hoge'];
+		const output = parser(input);
+		expect(output).toEqual<ParserOutput<'a'>>({
+			result: 'fail',
+		});
+	});
+	test('input "apply"', () => {
+		const input = [...'apply'];
+		const output = parser(input);
+		expect(output).toEqual<ParserOutput<'a'>>({
+			result: 'success',
+			data: 'a',
+			rest: [...'pply'],
+		});
+	});
+});
+
+describe('is()', () => {
+	describe('is(c => c == "a")', () => {
+		const parser = is((c): c is 'a' => c === 'a');
+
+		test('Empty input', () => {
+			const input = [] as const;
+			const output = parser(input);
+			expect(output).toEqual<ParserOutput<'a'>>({
+				result: 'fail',
+			});
+		});
+		test('input "a"', () => {
+			const input = [...'a'];
+			const output = parser(input);
+			expect(output).toEqual<ParserOutput<'a'>>({
+				result: 'success',
+				data: 'a',
+				rest: [],
+			});
+		});
+		test('input "A"', () => {
+			const input = [...'A'];
+			const output = parser(input);
+			expect(output).toEqual<ParserOutput<'a'>>({
+				result: 'fail',
+			});
+		});
+	});
+	describe('is(c => c === "0" || c === "1")', () => {
+		const parser = is((c): c is '0' | '1' => c === '0' || c === '1');
+
+		test('Empty input', () => {
+			const input = [] as const;
+			const output = parser(input);
+			expect(output).toEqual<ParserOutput<'0' | '1'>>({
+				result: 'fail',
+			});
+		});
+		test('input "0"', () => {
+			const input = [...'0'];
+			const output = parser(input);
+			expect(output).toEqual<ParserOutput<'0' | '1'>>({
+				result: 'success',
+				data: '0',
+				rest: [],
+			});
+		});
+		test('input "1"', () => {
+			const input = [...'1'];
+			const output = parser(input);
+			expect(output).toEqual<ParserOutput<'0' | '1'>>({
+				result: 'success',
+				data: '1',
+				rest: [],
+			});
+		});
+		test('input "A"', () => {
+			const input = [...'A'];
+			const output = parser(input);
+			expect(output).toEqual<ParserOutput<'0' | '1'>>({
+				result: 'fail',
+			});
+		});
+	});
+});
---
+++ src/character.ts
@@ -0,0 +1,40 @@
+import { anyChar } from './primitives';
+import { Parser, ParserInput } from 'types';
+
+// 任意の1文字のみのパーサ
+type CharFunc = <T extends ParserInput[0]>(c: T) => Parser<T>;
+export const character: CharFunc = (c) => {
+	return (input) => {
+		const r = anyChar(input);
+		if (r.result === 'fail') {
+			return r;
+		}
+		if (r.data !== c) {
+			return { result: 'fail' };
+		}
+		return {
+			result: 'success',
+			data: c,
+			rest: r.rest,
+		};
+	};
+};
+
+// 条件を満たす1文字のパーサ
+type IsFunc = <T extends string>(func: (c: ParserInput[0]) => c is T) => Parser<string>;
+export const is: IsFunc = (func) => {
+	return (input) => {
+		const r = anyChar(input);
+		if (r.result === 'fail') {
+			return r;
+		}
+		if (func(r.data) === false) {
+			return { result: 'fail' };
+		}
+		return {
+			result: 'success',
+			data: r.data,
+			rest: r.rest,
+		};
+	};
+};
